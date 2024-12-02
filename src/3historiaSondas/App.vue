34454e5bbc18cf8a669e2e2d4740fac5c4234a22
SoJoaomesmo <4540746781@estudante.sed.sc.gov.br>
2024-12-02 20:28:33 -0300

    Início da página de história

--- src/3historiaSondas/App.vue
+++ src/3historiaSondas/App.vue
@@ -25,6 +25,36 @@
 
   <div class="seta"></div>
 
+<body>
+<section class="div-esq">
+  <div>
+  <h1 class="title1">ORIGENS</h1>
+  <h2 class="title2">GUERRA FRIA</h2>
+  </div>
+  <center><hr></center>
+</section>
+  <p class="text1">ALorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis, arcu nec pretium lacinia, dolor dui lobortis nunc, sed condimentum libero orci ut odio. In hac habitasse platea dictumst. Suspendisse eget orci elementum, porttitor turpis sed, ultrices dui. Vestibulum consequat augue ac mi feugiat dictum. Cras ac efficitur lorem, in ultricies tellus. Duis accumsan nulla eu pretium mattis. Vestibulum ac sem bibendum dui ornare cursus ut nec tortor. Curabitur vel erat quis diam semper</p>
+  <img src="../../public/assets/guerrafria.png" class="imagem1">
+
+<section class="div-dir">
+  <center><hr></center>
+  <div>
+  <h1 class="title3">CARL SAGAN</h1>
+  <h2 class="title4">A FIGURA PRINCIPAL</h2>
+  </div>
+</section>
+  <p class="text2">ALorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis, arcu nec pretium lacinia, dolor dui lobortis nunc, sed condimentum libero orci ut odio. In hac habitasse platea dictumst. Suspendisse eget orci elementum, porttitor turpis sed, ultrices dui. Vestibulum consequat augue ac mi feugiat dictum. Cras ac efficitur lorem, in ultricies tellus. Duis accumsan nulla eu pretium mattis. Vestibulum ac sem bibendum dui ornare cursus ut nec tortor. Curabitur vel erat quis diam semper</p>
+  <img src="../../public/assets/carlsagan.png" class="imagem2">
+
+  <img src="../../public/assets/DiscoOuro2.png" class="imagem3">
+  <div class="div1">
+    <h1 class="title5">DISCO DE OURO</h1>
+    <h1 class="title6">UMA MENSAGEM PARA O UNIVERSO</h1>
+  </div>
+  <div class="div2"><p class="text3">
+    ALorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis, arcu nec pretium lacinia, dolor dui lobortis nunc, sed condimentum libero orci ut odio. In hac habitasse platea dictumst. Suspendisse eget orci elementum, porttitor turpis sed, ultrices dui. Vestibulum consequat augue ac mi feugiat dictum. Cras ac efficitur lorem, in ultricies tellus. Duis accumsan nulla eu pretium mattis. Vestibulum ac sem bibendum dui ornare cursus ut nec tortor. Curabitur vel erat quis diam semper</p>
+    <img src="../../public/assets/cosmos.png" class="imagem4">
+  </div></body>
     <footer>
     <div>
       <h1>Sobre a Beyond<span>Galaxy</span></h1>
@@ -56,5 +86,147 @@ export default {
 </script>
 
 <style scoped>
+.seta{
+  margin-bottom: -20%;
+}
 
+body *{
+    background-color: #111C41;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Onest", sans-serif;
+    font-weight: 100;
+}
+
+.title1{
+    color: white;
+    font-size: 2.9rem;
+    margin-left: 5.5rem;
+    margin-top: 10rem;
+    font-weight: 235;
+}
+.title2{
+    color: #C898EC;
+    font-size: 1.8rem;
+    margin-left: 6rem;
+    margin-top: -0.4rem;
+    font-weight: 175;
+}
+.text1{
+    color: white;
+    font-size: 1.2rem;
+    margin-left: 5.5rem;
+    margin-top: 2rem;
+    width: 32rem;
+}
+.imagem1{
+    margin-left: 43.5rem;
+    margin-top: -28rem;
+    width: 35.5rem;
+    height: auto;
+}
+
+.title3{
+    color: white;
+    font-size: 2.9rem;
+    margin-left: 0.5rem;
+    margin-top: 5rem;
+    font-weight: 235;
+}
+.title4{
+    color: #C898EC;
+    font-size: 1.85rem;
+    margin-left: 0.9rem;
+    margin-top: -0.4rem;
+    font-weight: 175;
+}
+.text2{
+    color: white;
+    font-size: 1.2rem;
+    margin-left: 48.5rem;
+    margin-top: 2rem;
+    width: 32rem;
+}
+.imagem2{
+    margin-left: 5.5rem;
+    margin-top: -28rem;
+    width: 35.5rem;
+    height: auto;
+}
+
+.imagem3{
+    margin-top: 5rem;
+    width: 100%;
+    height: auto;
+}
+
+.div1{
+    background-color: #080E22;
+    height: 15rem;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    margin-top: -0.3rem;
+}
+.title5{
+    background-color: #080E22;
+    color: white;
+    margin-top: 5rem;
+    font-size: 2.9rem;
+    text-align: center;
+    font-weight: 235;
+}
+.title6{
+    background-color: #080E22;
+    color: #2552EA;
+    margin-top: 10rem;
+    font-size: 2rem;
+    margin-left: -32%;
+    text-align: center;
+    font-weight: 235;
+}
+.text3{
+    background-color: #080E22;
+    color: white;
+    font-size: 1.2rem;
+    margin: 4% 5% 5% 0;
+    width: 32rem;
+}
+.div2{
+    display: flex;
+    background-color: #080E22;
+    justify-content: center;
+    margin-top: -2.3rem;
+}
+
+.imagem4{
+  margin-bottom: 5%
+}
+
+.div-esq hr{ 
+    height: 0.5625rem;
+    background-color: #C898EC;
+    margin: 90% 0 0 10%;
+    width: 13.125rem;
+    border: none;
+    border-radius: 10px;
+}
+
+.div-dir hr{ 
+    height: 0.5625rem;
+    background-color: #C898EC;
+    margin: 55% 0 0 335%;
+    width: 13.125rem;
+    border: none;
+    border-radius: 10px;
+}
+
+.div-dir div{
+  margin-left: 45rem;
+}
+
+.div-esq, .div-dir{
+  display: flex;
+}
 </style>
