84102b01ad1eecbc9f95d0bf79d40f2c0bf2f86b
MauricioReisdoefer <mauricio.reisdoefer2009@gmail.com>
2024-12-16 21:13:03 -0300

    Consertando partes da página principal

--- src/1paginaInicial/App.vue
+++ src/1paginaInicial/App.vue
@@ -46,18 +46,13 @@
     </p>
 
     <div class="allimagesdiv">
-      <div class="singlediv" data-text="
-A grande conjunção é um fenômeno astronômico raro e fascinante, marcado pelo alinhamento dos planetas Júpiter e Saturno em suas órbitas ao redor do Sol. Esse evento ocorre aproximadamente a cada 20 anos, quando os dois maiores planetas do Sistema Solar se aproximam visualmente no céu noturno, chegando a parecer quase um único ponto luminoso para observadores da Terra. Essa proximidade aparente se dá devido à diferença de tempo que cada planeta leva para completar uma volta ao redor do Sol: Júpiter, cerca de 12 anos, e Saturno, aproximadamente 30 anos.">
+      <div class="singlediv">
         <img class='image' src="../../public/assets/Jupiter.jpg"/>
       </div>
-      <div class="singlediv" data-text="A Voyager 1, lançada em 1977, é a sonda espacial mais distante da Terra. Após explorar Júpiter e Saturno, revelou detalhes importantes, como a composição dos anéis de Saturno e a complexidade das luas Titã e Io. Em 2012, tornou-se o primeiro objeto humano a alcançar o espaço interestelar, atravessando a heliopausa. Carrega o Golden Record, um disco com sons, imagens e mensagens da Terra, destinado a possíveis civilizações extraterrestres. Continuando sua jornada, a Voyager 1 envia dados sobre o ambiente interestelar, contribuindo para a compreensão das fronteiras do Sistema Solar e da interação com o meio interestelar.">
+      <div class="singlediv">
         <img class="image" src="../../public/assets/voyager2.png"/>
       </div>
-      <div class="singlediv" data-text="O Disco de Ouro é um artefato cultural e científico levado pelas sondas Voyager 1 e 2, lançadas em 1977. Projetado por Carl Sagan e sua equipe, o disco é uma cápsula do tempo destinada a comunicar a diversidade da vida e da cultura na Terra a possíveis civilizações extraterrestres.
-
-Ele contém 116 imagens, saudações em 55 idiomas, sons da natureza, como o canto de pássaros e ondas do mar, além de uma seleção musical que inclui obras de Bach, Beethoven e músicas tradicionais de diversas culturas. O disco, feito de cobre banhado a ouro, está protegido por uma capa que traz instruções para sua reprodução e a localização da Terra no cosmos.
-
-Essa mensagem interestelar é um testemunho do desejo humano de conexão e exploração, representando nossa curiosidade e esperança de transcender as fronteiras do Sistema Solar.">
+      <div class="singlediv">
         <img class="image" src="../../public/assets/DiscoOuro.jpg"/>
       </div>
     </div>
@@ -65,17 +60,24 @@ Essa mensagem interestelar é um testemunho do desejo humano de conexão e explo
     <div class="topic-txt">
       <hr>
       <div>
-      <h3>A Grande Conjunção</h3>
-      <div class="divtext1">
-        <p id="texto">
-        </p>
+      <div id="text-container" class="hidden">
+          <h3 id="titulo">A Grande Conjunção</h3>
+          <div class="divtext1">
+              <p id="texto"></p>
+          </div>
       </div>
+
+      <div class="buttons-container">
+          <div class="singlediv" onclick="changeContent(1)">Botão 1</div>
+          <div class="singlediv" onclick="changeContent(2)">Botão 2</div>
+          <div class="singlediv" onclick="changeContent(3)">Botão 3</div>
+      </div>
+      </div>
+
       
       </div>
       <button><a href="historia">Veja mais</a></button>
     </div>
-  </div>
-
   <div class="hub">
     <div class="sub">
       <h1>Nosso jogo</h1>
@@ -154,10 +156,14 @@ export default {
     border: 0.1rem solid rgb(255, 255, 255);
 }
 
-.clicked {
+.singlediv.active {
     margin-left: 2rem;
     width: 30rem;
     border: 0.3rem solid rgb(255, 255, 255);
+}
+
+.hidden {
+    display: none;
 }
 
   .intro {
